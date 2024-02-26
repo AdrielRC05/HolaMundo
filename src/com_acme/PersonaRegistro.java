bf08e2160f3b9b96e1789b4976a18382cab5b046
adriel <adriel.ruano@iessanmamede.com>
2024-02-26 09:11:15 +0100

    Se creó el registro Persona en main (master)

---
+++ src/com_acme/PersonaRegistro.java
@@ -0,0 +1,10 @@
+package com_acme;
+
+public record PersonaRegistro(String nombre, String apellidos){
+
+    @Override
+    public String toString(){
+        return "%s %s".formatted(nombre, apellidos);
+    }
+}
+
