b667c3f9555c73d8710540bd8597c213cc5d4cde
adriel <adriel.ruano@iessanmamede.com>
2024-03-06 09:26:19 +0100

    Se quitó una línea inservible

--- src/com_acme/PersonaRegistro.java
+++ src/com_acme/PersonaRegistro.java
@@ -4,7 +4,6 @@ public record PersonaRegistro(String nombre, String apellidos){
 
     @Override
     public String toString(){
-        //Se comentó una línea inservible
         System.out.println("Esta instrucción es inservible");
         return "%s %s".formatted(nombre, apellidos);
     }
